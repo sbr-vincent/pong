e0702f4b95292663f00e0d8a9386272a4aafd824
sbr-vincent <vincent.g.sbr@gmail.com>
2024-02-19 16:02:54 -0800

    Initial commit

---
+++ paddle.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(position)
+
+    def go_up(self):
+        if self.ycor() < 240:
+            new_y = self.ycor() + 30
+            self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        if self.ycor() > -240:
+            new_y = self.ycor() - 30
+            self.goto(self.xcor(), new_y)
+
+
